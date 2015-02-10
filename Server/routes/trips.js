ec07d51635058d7c421d5d9f3bb1dfbb32281743
William Henderson <william.c.henderson@gmail.com>
2015-02-10 14:01:53 -0800

    simple response caching

--- Server/routes/trips.js
+++ Server/routes/trips.js
@@ -1,38 +1,52 @@
 var db = require('../db.js');
 var utils = require('../utils/utils.js');
+var NodeCache = require( "node-cache" );
+var tripCache = require('memory-cache');
 
 exports.getAll = function(req, res){
-  var trips = db.mongo_client.get('trips');
-    
-  trips.find({},{w:1},function(error,trips) {
-		if(error){
-			res.status(404).send('Not found');
-			console.error(error);    
-		} else {
-		  var geojson = { "type": "FeatureCollection",
-          "features": []
-           };
-		  for(i=0; i<trips.length; i++) {
-        var trip = trips[i];
-        geojson.features.push({  
-    			"type": "Feature",
-    			"geometry": {
-            "type": "LineString",
-            "coordinates": trip.locations.map(function(loc) {return loc.pos.reverse()})
-          },
-    			"properties": {
-    				"id"					:trip._id,
-    				"activity_type" : trip.activityType,
-    				"creation_date" : trip.creationdate,
-    				"rating" : trip.rating,
-    				"uuid" : trip.uuid
-    			}						
-    		});
-    	}	
-      
-		  return res.json(geojson);
-		}
-	});
+  var cachedTrips = tripCache.get("allTrips");
+  
+  if(cachedTrips){
+      console.log("using cached response!");
+      return res.json(cachedTrips);
+  } else {
+      var trips = db.mongo_client.get('trips');
+
+      trips.find({},{w:1},function(error,trips) {
+    		if(error){
+    			res.status(404).send('Not found');
+    			console.error(error);    
+    		} else {
+    		  var geojson = { "type": "FeatureCollection",
+              "features": []
+               };
+    		  for(i=0; i<trips.length; i++) {
+            var trip = trips[i];
+            geojson.features.push({  
+        			"type": "Feature",
+        			"geometry": {
+                "type": "LineString",
+                "coordinates": trip.locations.map(function(loc) {return loc.pos.reverse()})
+              },
+        			"properties": {
+        				"id"					:trip._id,
+        				"activity_type" : trip.activityType,
+        				"creation_date" : trip.creationdate,
+        				"rating" : trip.rating,
+        				"uuid" : trip.uuid
+        			}						
+        		});
+        	}	
+
+          console.log("caching response!");
+          
+          // cache for two hours
+        	tripCache.put("allTrips", geojson, 120*60*1000);
+
+    		  return res.json(geojson);
+    		}
+    	});
+  }
 };
 
 exports.getTripsOnDate = function(req, res){
