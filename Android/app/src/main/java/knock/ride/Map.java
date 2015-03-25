82af1b6fc66f9ed8ceeebe091a96ec2871921d81
William Henderson <william.c.henderson@gmail.com>
2015-03-24 18:02:02 -0700

    android hacking

--- Android/app/src/main/java/knock/ride/Map.java
+++ Android/app/src/main/java/knock/ride/Map.java
@@ -1,33 +1,38 @@
 package knock.ride;
 
-        import android.app.AlertDialog;
-        import android.content.DialogInterface;
-        import android.content.Intent;
-        import android.net.Uri;
-        import android.os.Bundle;
-        import android.provider.Settings;
-        import android.support.v7.app.ActionBarActivity;
-        import android.text.TextUtils;
-        import android.view.Menu;
-        import android.view.MenuInflater;
-        import android.view.MenuItem;
-        import android.view.View;
-        import android.widget.Button;
-        import com.mapbox.mapboxsdk.api.ILatLng;
-        import com.mapbox.mapboxsdk.geometry.BoundingBox;
-        import com.mapbox.mapboxsdk.geometry.LatLng;
-        import com.mapbox.mapboxsdk.overlay.Icon;
-        import com.mapbox.mapboxsdk.overlay.Marker;
-        import com.mapbox.mapboxsdk.overlay.UserLocationOverlay;
-        import com.mapbox.mapboxsdk.tileprovider.tilesource.*;
-        import com.mapbox.mapboxsdk.views.MapView;
-        import com.mapbox.mapboxsdk.views.util.TilesLoadedListener;
+import android.content.IntentSender;
+import android.location.Location;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
+import android.view.View;
 
-public class Map extends ActionBarActivity {
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+
+import com.mapbox.mapboxsdk.api.ILatLng;
+import com.mapbox.mapboxsdk.geometry.LatLng;
+import com.mapbox.mapboxsdk.overlay.Icon;
+import com.mapbox.mapboxsdk.overlay.Marker;
+import com.mapbox.mapboxsdk.overlay.UserLocationOverlay;
+import com.mapbox.mapboxsdk.tileprovider.tilesource.MapboxTileLayer;
+import com.mapbox.mapboxsdk.views.MapView;
+import com.mapbox.mapboxsdk.views.util.TilesLoadedListener;
+
+public class Map extends ActionBarActivity implements
+        GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener, LocationListener {
 
     private MapView mv;
     private UserLocationOverlay myLocationOverlay;
     private String currentMap = null;
+    private GoogleApiClient mGoogleApiClient;
+    public static final String TAG = Map.class.getSimpleName();
+    private final static int CONNECTION_FAILURE_RESOLUTION_REQUEST = 9000;
+    private LocationRequest mLocationRequest;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,7 +51,19 @@ public class Map extends ActionBarActivity {
         // Show user location (purposely not in follow mode)
         mv.setUserLocationEnabled(true);
 
-        mv.loadFromGeoJSONURL("http://ride.report/trips");
+//        mv.loadFromGeoJSONURL("http://ride.report/trips");
+
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .build();
+
+        // Create the LocationRequest object
+        mLocationRequest = LocationRequest.create()
+                .setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY)
+                .setInterval(10 * 1000)        // 10 seconds, in milliseconds
+                .setFastestInterval(1 * 1000); // 1 second, in milliseconds
 
 
         mv.setOnTilesLoadedListener(new TilesLoadedListener() {
@@ -64,6 +81,21 @@ public class Map extends ActionBarActivity {
         mv.setVisibility(View.VISIBLE);
     }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mGoogleApiClient.connect();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (mGoogleApiClient.isConnected()) {
+            LocationServices.FusedLocationApi.removeLocationUpdates(mGoogleApiClient, this);
+            mGoogleApiClient.disconnect();
+        }
+    }
+
 
     public LatLng getMapCenter() {
         return mv.getCenter();
@@ -71,5 +103,54 @@ public class Map extends ActionBarActivity {
 
     public void setMapCenter(ILatLng center) {
         mv.setCenter(center);
+    }
+
+    private void handleNewLocation(Location location) {
+        Log.d(TAG, location.toString());
+        double currentLatitude = location.getLatitude();
+        double currentLongitude = location.getLongitude();
+        LatLng latLng = new LatLng(currentLatitude, currentLongitude);
+        Marker m = new Marker(mv, "Current Location", "Current Location", latLng);
+        mv.addMarker(m);
+
+        mv.setCenter(latLng);
+    }
+
+    @Override
+    public void onConnected(Bundle bundle) {
+        Log.i(TAG, "Location services connected.");
+        Location location = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+        if (location == null) {
+            LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, this);
+        }
+        else {
+            handleNewLocation(location);
+        };
+
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+        Log.i(TAG, "Location services suspended. Please reconnect.");
+
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+        if (connectionResult.hasResolution()) {
+            try {
+                // Start an Activity that tries to resolve the error
+                connectionResult.startResolutionForResult(this, CONNECTION_FAILURE_RESOLUTION_REQUEST);
+            } catch (IntentSender.SendIntentException e) {
+                e.printStackTrace();
+            }
+        } else {
+            Log.i(TAG, "Location services connection failed with code " + connectionResult.getErrorCode());
+        }
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        handleNewLocation(location);
     }
 }
