6b8f2c0541101ca13807032f2e27707b39724753
William Henderson <william.c.henderson@gmail.com>
2015-08-11 14:02:46 -0700

    talk to new api endpoints, use authenticated API's

--- App/Ride/APIClient.swift
+++ App/Ride/APIClient.swift
@@ -13,9 +13,9 @@ import OAuthSwift
 import Locksmith
 
 #if (arch(i386) || arch(x86_64)) && os(iOS)
-let serverAddress = "http://127.0.0.1:8080/api/"
+let serverAddress = "http://192.168.1.32:8000/api/v2/"
 #else
-let serverAddress = "http://beta.ride.report/api/"
+let serverAddress = "http://api.ride.report/api/v2/"
 #endif
     
 class APIClient {
@@ -46,18 +46,12 @@ class APIClient {
     
     func startup() {
         dispatch_async(dispatch_get_main_queue(), { () -> Void in
-            for aTrip in Trip.openTrips()! {
-                let trip = aTrip as! Trip
-                
-                if (trip.locations.count <= 6) {
-                    // if it doesn't more than 6 points, toss it.
-                    CoreDataManager.sharedManager.currentManagedObjectContext().deleteObject(trip)
-                    self.saveAndSyncTripIfNeeded(trip)
-                } else {
-                    trip.close() {
-                        self.saveAndSyncTripIfNeeded(trip)
-                    }
-                }
+            if (!self.authenticated) {
+                self.authenticate(successHandler: {
+                    self.syncTrips(syncInBackground: false)
+                })
+            } else {
+                self.syncOpenTrips()
             }
             
         })
@@ -84,6 +78,22 @@ class APIClient {
     //
     // MARK: - Trip Synchronization
     //
+    
+    func syncOpenTrips(){
+        for aTrip in Trip.openTrips()! {
+            let trip = aTrip as! Trip
+            
+            if (trip.locations.count <= 6) {
+                // if it doesn't more than 6 points, toss it.
+                CoreDataManager.sharedManager.currentManagedObjectContext().deleteObject(trip)
+                self.saveAndSyncTripIfNeeded(trip)
+            } else {
+                trip.close() {
+                    self.saveAndSyncTripIfNeeded(trip)
+                }
+            }
+        }
+    }
     
     func syncTrips(syncInBackground: Bool = false) {
         dispatch_async(dispatch_get_main_queue(), { () -> Void in
@@ -148,12 +158,11 @@ class APIClient {
             ])
         }
         tripDict["locations"] = locations
-        
-        tripDict["incidents"] = incidents
-        self.makeAuthenticatedRequest(Alamofire.Method.POST, route: "trips/save", parameters: tripDict).validate().response { (request, response, data, error) in
+
+        self.makeAuthenticatedRequest(Alamofire.Method.POST, route: "save_trip", parameters: tripDict).validate().response { (request, response, data, error) in
             if (error == nil) {
                 trip.isSynced = true
-                DDLogError(String(format: "Response: %@", response!))
+                DDLogInfo(String(format: "Response: %@", response!))
                 CoreDataManager.sharedManager.saveContext()
             } else {
                 DDLogError(String(format: "Error: %@", error!))
@@ -179,8 +188,18 @@ class APIClient {
     }
     
     
-    private func makeAuthenticatedRequest(method: Alamofire.Method, route: String, parameters: [String: AnyObject]? = nil) -> Request {
-        return self.manager.request(method, serverAddress + route, parameters: parameters, encoding: .JSON, headers: self.requestHeaders)
+    private func makeAuthenticatedRequest(method: Alamofire.Method, route: String, parameters: [String: AnyObject]? = nil, encoding: ParameterEncoding = .JSON) -> Request {
+        return self.manager.request(method, serverAddress + route, parameters: parameters, encoding: encoding, headers: self.requestHeaders).response { (request, response, data, error) in
+            if (response?.statusCode == 401) {
+                dispatch_async(dispatch_get_main_queue(), { () -> Void in
+                    if (self.authenticated) {
+                        let alert = UIAlertView(title:nil, message: "There was an authenication error talking to the server. Please report this issue to logs@ride.report!", delegate: nil, cancelButtonTitle:"Sad Panda")
+                        alert.show()
+                    }
+                    self.reauthenticate()
+                })
+            }
+        }
     }
     
     //
@@ -191,23 +210,34 @@ class APIClient {
         return (self.accessToken != nil)
     }
     
-    func authenticate() {
+    func reauthenticate() {
+        if (!self.authenticated) {
+            // avoid duplicate reauthenticate requests
+            return
+        }
+        
+        if (self.deleteAccessToken()) {
+            self.authenticate()
+        }
+    }
+    
+    func authenticate(successHandler: ()->Void = {}) {
         let uuid = Profile.profile().uuid
         var parameters = ["client_id" : "1ARN1fJfH328K8XNWA48z6z5Ag09lWtSSVRHM9jw", "response_type" : "token"]
         parameters["uuid"] = uuid
-        self.makeAuthenticatedRequest(Alamofire.Method.GET, route: "oauth_token", parameters: parameters).validate().responseJSON(options: nil) { (request, response, jsonData, error) -> Void in
+        self.makeAuthenticatedRequest(Alamofire.Method.GET, route: "oauth_token", parameters: parameters, encoding: .URL).validate().responseJSON(options: nil) { (request, response, jsonData, error) -> Void in
             if (error == nil) {
                 // do stuff with the response
                 let data = JSON(jsonData!)
                 if let accessToken = data["access_token"].string, expiresIn = data["expires_in"].string {
-                    self.saveAccessToken(accessToken, expiresIn: expiresIn)
+                    if(self.saveAccessToken(accessToken, expiresIn: expiresIn)) {
+                        successHandler()
+                    }
                 }
             } else {
                 DDLogError(String(format: "Error retriving access token: %@", error!))
             }
         }
-        
-        APIClient.sharedClient.testAuthID()
     }
     
     func testAuthID() {
@@ -222,11 +252,8 @@ class APIClient {
         }
     }
 
-    private func saveAccessToken(token: String, expiresIn: String) {
-        let deleteRequest = LocksmithRequest(userAccount: rideKeychainUserName, requestType: RequestType.Delete)
-        deleteRequest.synchronizable = true
-        deleteRequest.accessible = Accessible.AfterFirstUnlockThisDeviceOnly
-        Locksmith.performRequest(deleteRequest)
+    private func saveAccessToken(token: String, expiresIn: String) -> Bool {
+        self.deleteAccessToken()
         
         let saveTokenRequest = LocksmithRequest(userAccount: rideKeychainUserName, requestType: RequestType.Create, data: ["accessToken" : token, "expiresIn" : expiresIn])
         saveTokenRequest.synchronizable = true
@@ -235,10 +262,31 @@ class APIClient {
         
         if (error != nil) {
             DDLogError(String(format: "Error storing access token: %@", error!))
+            return false
         } else {
             // make sure any old access token isn't memoized
             _hasLookedForAccessToken = false
             _accessToken = nil
+            
+            return true
+        }
+    }
+    
+    private func deleteAccessToken() -> Bool {
+        let deleteRequest = LocksmithRequest(userAccount: rideKeychainUserName, requestType: RequestType.Delete)
+        deleteRequest.synchronizable = true
+        deleteRequest.accessible = Accessible.AfterFirstUnlockThisDeviceOnly
+        let (dictionary, error) = Locksmith.performRequest(deleteRequest)
+        
+        if (error != nil) {
+            DDLogError(String(format: "Error delete access token: %@", error!))
+            return false
+        } else {
+            // make sure any old access token isn't memoized
+            _hasLookedForAccessToken = false
+            _accessToken = nil
+            
+            return true
         }
     }
     
