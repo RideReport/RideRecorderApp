f47d0a3679b88f80727ffc963f009c1c8d2bc9f3
William Henderson <william.c.henderson@gmail.com>
2015-12-15 17:31:42 -0800

    Revert "re-enable software update manager"
    This reverts commit 3944621d64f89cdedcd5653e20d0f512aea8988e.

--- App/Ride/AppDelegate.swift
+++ App/Ride/AppDelegate.swift
@@ -58,7 +58,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UIAlertViewDelegate {
         // Start Managers. Note that order matters!
         CoreDataManager.startup()
         APIClient.startup()
-        SoftwareUpdateManager.startup()
 //        HealthKitManager.startup()
         
         if (hasSeenSetup) {
