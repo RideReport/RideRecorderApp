3f8d85e4c3f5ea3840e5ec24417d58ee8fabcb36
William Henderson <william.c.henderson@gmail.com>
2016-02-12 18:27:43 -0800

    fix iOS 8.4 crasher from sprite kit

---
+++ App/Ride/NSString+HBAdditions.swift
@@ -0,0 +1,39 @@
+//
+//  NSString+HBAdditions.swift
+//  Ride
+//
+//  Created by William Henderson on 2/12/16.
+//  Copyright © 2016 Knock Softwae, Inc. All rights reserved.
+//
+
+import Foundation
+
+extension Character {
+    func isUnsupportedEmoji() -> Bool {
+        let chars = Array(String(self).utf16)
+        let size = chars.count
+        let font = UIFont(name: "AppleColorEmoji", size: 64)!
+        var glyphs = Array<CGGlyph>(count: size, repeatedValue: 0)
+        let supported = CTFontGetGlyphsForCharacters(font, chars, &glyphs, size)
+        
+        return !supported
+    }
+}
+
+extension String {
+    func containsUnsupportEmoji()->Bool {
+        for c in self.characters {
+            if (c.isUnsupportedEmoji()) {
+                return true
+            }
+        }
+        
+        return false
+    }
+    
+    func stringByRemovingUnsupportedEmoji() -> String {
+        return String(self.characters.filter(){ (char) -> Bool in
+            return !char.isUnsupportedEmoji()
+        })
+    }
+}
