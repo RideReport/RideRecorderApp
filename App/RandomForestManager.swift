89756a261b5e9abca33fb42170e10a15c53d846e
William Henderson <william.c.henderson@gmail.com>
2016-03-16 16:40:35 -0700

    cleanup RandomForestManager/FFTManager

--- App/RandomForestManager.swift
+++ App/RandomForestManager.swift
@@ -10,20 +10,38 @@ import Foundation
 
 class RandomForestManager {
     var _ptr: COpaquePointer
+    var classLables: [Int32]!
+    var classCount = 0
     
     init(sampleSize: Int) {
         let path = NSBundle(forClass: self.dynamicType).pathForResource("forest.cv", ofType: nil)
         let cpath = path?.cStringUsingEncoding(NSUTF8StringEncoding)
 
         _ptr = createRandomForestManager(Int32(sampleSize), UnsafeMutablePointer(cpath!))
+        self.classCount = Int(randomForestGetClassCount(_ptr))
+        self.classLables = [Int32](count:self.classCount, repeatedValue:0)
+        randomForestGetClassLabels(_ptr, UnsafeMutablePointer(self.classLables), Int32(self.classCount))
     }
     
     deinit {
         deleteRandomForestManager(_ptr)
     }
     
-    func classifyMagnitudeVector(magnitudeVector: [Float], speedVector: [Float])->Int
+    func classifyMagnitudeVector(magnitudeVector: [Float])->(sampleClass: Int, confidence: Float)
     {
-        return Int(randomForesetClassifyMagnitudeVector(_ptr, UnsafeMutablePointer(magnitudeVector), UnsafeMutablePointer(speedVector), Int32(speedVector.count)))
+        let confidences = [Float](count:self.classCount, repeatedValue:0.0)
+        randomForestClassificationConfidences(_ptr, UnsafeMutablePointer(magnitudeVector), UnsafeMutablePointer(confidences), 4)
+        
+        var highScore: Float = 0
+        var highScoreIndex = 0
+        
+        for (i, score) in confidences.enumerate() {
+            if score > highScore {
+                highScore = score
+                highScoreIndex = i
+            }
+        }
+        
+        return (Int(classLables[highScoreIndex]), highScore)
     }
 }
