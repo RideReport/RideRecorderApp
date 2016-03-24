62b7e30afe4f5ed96591023fbfbea7f8b3b4ed2e
William Henderson <william.c.henderson@gmail.com>
2016-03-24 15:40:40 -0700

    fix layout issue

--- App/Ride/RoutesViewController.swift
+++ App/Ride/RoutesViewController.swift
@@ -572,7 +572,7 @@ class RoutesViewController: UIViewController, UITableViewDataSource, UITableView
             var tabStops : [NSTextTab] = []
             var totalLineWidth : CGFloat = 0
             var columnCount = 0
-            while totalLineWidth + totalWidth < (self.view.frame.size.width - 10) {
+            while totalLineWidth + totalWidth < (self.view.frame.size.width - 12) {
                 tabStops.append(NSTextTab(textAlignment: NSTextAlignment.Center, location: totalLineWidth + emojiWidth , options: [NSTabColumnTerminatorsAttributeName:NSCharacterSet(charactersInString:"x")]))
                 tabStops.append(NSTextTab(textAlignment: NSTextAlignment.Right, location: totalLineWidth + emojiWidth + crossWidth + countWidth, options: [:]))
                 tabStops.append(NSTextTab(textAlignment: NSTextAlignment.Left, location: totalLineWidth + emojiWidth + crossWidth + countWidth + columnSeperatorWidth, options: [:]))
