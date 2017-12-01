6e9e9f85fcf12c49103a9c84fa3bbbcfdff51a7d
William Henderson <william.c.henderson@gmail.com>
2017-11-30 16:07:30 -0800

    dont call sync trips multiple times

--- App/Ride/TripsViewController.swift
+++ App/Ride/TripsViewController.swift
@@ -165,6 +165,14 @@ class TripsViewController: UIViewController, UITableViewDataSource, UITableViewD
             strongSelf.refreshHeaderCells()
         }
         
+        RideReportAPIClient.shared.syncTrips()
+        DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + Double(Int64(0.4 * Double(NSEC_PER_SEC))) / Double(NSEC_PER_SEC), execute: { () -> Void in
+            // avoid a bug that could have this called twice on app launch
+            NotificationCenter.default.addObserver(forName: NSNotification.Name.UIApplicationDidBecomeActive, object: nil, queue: nil) { _ in
+                RideReportAPIClient.shared.syncTrips()
+            }
+        })
+        
         if RideReportAPIClient.shared.accountVerificationStatus != .unknown {
             self.runCreateAccountOfferIfNeeded()
         } else {
@@ -193,19 +201,6 @@ class TripsViewController: UIViewController, UITableViewDataSource, UITableViewD
                 self.present(alertController, animated: true, completion: nil)
             }
         }
-    }
-    
-    override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(animated)
-        
-        RideReportAPIClient.shared.syncTrips()
-        
-        DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + Double(Int64(0.1 * Double(NSEC_PER_SEC))) / Double(NSEC_PER_SEC), execute: { () -> Void in
-            // avoid a bug that could have this called twice on app launch
-            NotificationCenter.default.addObserver(forName: NSNotification.Name.UIApplicationDidBecomeActive, object: nil, queue: nil) { _ in
-                RideReportAPIClient.shared.syncTrips()
-            }
-        })
     }
     
     override func viewWillAppear(_ animated: Bool) {
