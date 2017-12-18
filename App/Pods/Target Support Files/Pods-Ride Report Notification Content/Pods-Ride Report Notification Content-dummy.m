a0821d590aec98ee7c058d7e6add2bdcfa3ad9de
William Henderson <william.c.henderson@gmail.com>
2017-12-18 08:50:25 -0800

    Add Pods to the repo

---
+++ App/Pods/Target Support Files/Pods-Ride Report Notification Content/Pods-Ride Report Notification Content-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Ride_Report_Notification_Content : NSObject
+@end
+@implementation PodsDummy_Pods_Ride_Report_Notification_Content
+@end
---
+++ App/Pods/Target Support Files/Pods-RouteRecorder/Pods-RouteRecorder-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_RouteRecorder : NSObject
+@end
+@implementation PodsDummy_Pods_RouteRecorder
+@end
