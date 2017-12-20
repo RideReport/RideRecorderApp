75bc7ecd99da9c62e4a72ce60ec3dc81acc46b02
William Henderson <william.c.henderson@gmail.com>
2017-12-19 17:20:47 -0800

    injection support

---
+++ App/Ride/UIViewController+Injection.swift
@@ -0,0 +1,39 @@
+//
+//  UIViewController+Injection.swift
+//  Ride
+//
+//  Created by William Henderson on 12/19/17.
+//  Copyright © 2017 Knock Softwae, Inc. All rights reserved.
+//
+
+import UIKit
+
+extension UIViewController {
+    
+    #if DEBUG
+    @objc func injected() {
+        for subview in self.view.subviews {
+            subview.removeFromSuperview()
+        }
+        
+        if let sublayers = self.view.layer.sublayers {
+            for sublayer in sublayers {
+                sublayer.removeFromSuperlayer()
+            }
+        }
+        
+        viewDidLoad()
+    }
+    #endif
+}
+
+extension UITableViewController {
+    #if DEBUG
+    @objc override func injected() {
+        super.injected()
+        self.tableView.reloadData()
+        
+        viewDidLoad()
+    }
+    #endif
+}
