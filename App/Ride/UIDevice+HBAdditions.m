a4be2d60d2e8ada8fcff73dc98daaf81af05db07
William Henderson <william.c.henderson@gmail.com>
2018-02-09 15:59:24 -0800

    remove old private code

--- App/Ride/UIDevice+HBAdditions.m
+++ App/Ride/UIDevice+HBAdditions.m
@@ -36,23 +36,6 @@
     return NO;
 }
 
-- (NSDictionary *)_usageStastics;
-{
-    NSDictionary *output = nil;
-    
-    void * handle = dlopen("/System/Library/PrivateFrameworks/PowerLog.framework/PowerLog", RTLD_LAZY);
-    NSDictionary *(*PLBatteryUsageUIQuery)(NSString *key, NSDictionary *dict) = dlsym (handle, "PLBatteryUsageUIQuery");
-    
-    if (PLBatteryUsageUIQuery) {
-        output = PLBatteryUsageUIQuery (@"PLBatteryUIQueryFunctionKey", @{@"PLBatteryUIQueryFunctionKey": @0});
-    }
-    if (handle) {
-        dlclose (handle);
-    }
-    
-    return output;
-}
-
 - (NSString*)deviceModel;
 {
     struct utsname systemInfo;
@@ -60,40 +43,6 @@
     
     return [NSString stringWithCString:systemInfo.machine
                               encoding:NSUTF8StringEncoding];
-}
-
-- (NSDictionary *)dailyUsageStasticsForBundleIdentifier:(NSString *)bundleID;
-{
-    NSDictionary *usage = [self _usageStastics];
-    if (!usage) {
-        return nil;
-    }
-    
-    NSArray *appsArray = [[usage objectForKey:@"PLBatteryUIQueryRangeDayKey"] objectForKey:@"PLBatteryUIAppArrayKey"];
-    for (NSDictionary *dict in appsArray) {
-        if ([[dict objectForKey:@"PLBatteryUIAppBundleIDKey"] isEqualToString:bundleID]) {
-            return dict;
-        }
-    }
-    
-    return nil;
-}
-
-- (NSDictionary *)weeklyUsageStasticsForBundleIdentifier:(NSString *)bundleID;
-{
-    NSDictionary *usage = [self _usageStastics];
-    if (!usage) {
-        return nil;
-    }
-    
-    NSArray *appsArray = [[usage objectForKey:@"PLBatteryUIQueryRangeWeekKey"] objectForKey:@"PLBatteryUIAppArrayKey"];
-    for (NSDictionary *dict in appsArray) {
-        if ([[dict objectForKey:@"PLBatteryUIAppBundleIDKey"] isEqualToString:bundleID]) {
-            return dict;
-        }
-    }
-    
-    return nil;
 }
 
 @end
