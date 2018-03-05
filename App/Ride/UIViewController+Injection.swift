0b3a6d5a42f0e1ce1f02e2e35ac114851f09fd98
William Henderson <william.c.henderson@gmail.com>
2018-03-05 15:53:18 -0800

    disable debug injection stuff for now

--- App/Ride/UIViewController+Injection.swift
+++ App/Ride/UIViewController+Injection.swift
@@ -8,32 +8,33 @@
 
 import UIKit
 
-extension UIViewController {
-    
-    #if DEBUG
-    @objc func injected() {
-        for subview in self.view.subviews {
-            subview.removeFromSuperview()
-        }
-        
-        if let sublayers = self.view.layer.sublayers {
-            for sublayer in sublayers {
-                sublayer.removeFromSuperlayer()
-            }
-        }
-        
-        viewDidLoad()
-    }
-    #endif
-}
+//extension UIViewController {
+//    
+//    #if DEBUG
+//    @objc func injected() {
+//        for subview in self.view.subviews {
+//            subview.removeFromSuperview()
+//        }
+//        
+//        if let sublayers = self.view.layer.sublayers {
+//            for sublayer in sublayers {
+//                sublayer.removeFromSuperlayer()
+//            }
+//        }
+//        
+//        viewDidLoad()
+//    }
+//    #endif
+//}
+//
+//extension UITableViewController {
+//    #if DEBUG
+//    @objc override func injected() {
+//        super.injected()
+//        self.tableView.reloadData()
+//        
+//        viewDidLoad()
+//    }
+//    #endif
+//}
 
-extension UITableViewController {
-    #if DEBUG
-    @objc override func injected() {
-        super.injected()
-        self.tableView.reloadData()
-        
-        viewDidLoad()
-    }
-    #endif
-}
